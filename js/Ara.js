81eec03bed4adfd834b390921016f3e6b5a98a46
caioever <caio.everest@hotmai.com>
2017-09-19 20:46:04 -0300

    Add formulario de contato ao html e corrigido pequenos erros nojs

--- js/Ara.js
+++ js/Ara.js
@@ -2,7 +2,7 @@
 //Função para smooth scroll
 $('a').not('.accordion').click(function(){
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
+        scrollTop: $( $(this).attr('href') ).offset().top - 80
     }, 500);
     return false;
 });
