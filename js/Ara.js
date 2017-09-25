f12b60aab35fab0c4735572ab074cb37e82f4362
caioeve <caio.everest@hotmail.com>
2017-09-25 07:44:21 -0300

    função load js

--- js/Ara.js
+++ js/Ara.js
@@ -1,4 +1,4 @@
-$(function() {	
+$(function() {
 	//Função para smooth scroll
 	$('a').not('.semSmooth').click(function(){
 		$('html, body').animate({
@@ -6,38 +6,45 @@ $(function() {
 		}, 500);
 		return false;
 	});
-	
+
+	//Função para carregar dinamicamente o navtop e o rodape
+	/*$.get("./assets/navtop.html", function(data) {
+		$("#headerNav").html(data);
+	});*/
+	//$("#headerNav").load("./assets/navtop.html");
+	//$("#footer").load("assets/rodape.html");
+
 	//Mascara Jquery
 	$("#foneContato").mask("(99) 9999-9999");
-	
+
 	//Regras de validação dos campos do form
     $("#formContato").validate({ //usa o name dos campos do form
 		ignore:'',
         rules: {
             emailContato:{required: true, email:true},
             nomeContato:{required: true, minlength: 4},
-			subjectContato:{required: true},           
+			subjectContato:{required: true},
             foneContato:{required: true},
 			msgContato:{required: true}
         },
-    
+
         messages: {
         	emailContato:{required: "Informe o email", email:"Email inválido"},
-        	nomeContato:{required: "*Informe o nome", 
+        	nomeContato:{required: "*Informe o nome",
                minlength:"*É nome, animal não é apelido não menos 4 caracteres"},
 			subjectContato:{required: "*Escolhe alguma opção brother, assim tu não coopera"},
-            foneContato:{required: "*Manda o numero ai pra gente conversar no Whats"},            
-            msgContato:{required:"*Digite alguma coisa ai mano, qual foi..."}            
+            foneContato:{required: "*Manda o numero ai pra gente conversar no Whats"},
+            msgContato:{required:"*Digite alguma coisa ai mano, qual foi..."}
         },
-		
+
 		errorPlacement: function ($erroFormContato, $element) {
             var nome = $element.attr("name");
             $("#erro_" + nome).append($erroFormContato);
         }
-	
+
     });
-	
-    //Limpar campos de um form quando o usuário clicar no botão Limpar 
+
+    //Limpar campos de um form quando o usuário clicar no botão Limpar
     $(".btlimpar").click(function(){
     	$('#formContato').each (function(){
     		  this.reset();
