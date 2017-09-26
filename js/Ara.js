71248e236f65841028fd8f4bdc81a02a2783a1aa
caioever <caio.everest@hotmai.com>
2017-09-25 23:44:26 -0300

    Add validador JS para o newDoc na admin page

--- js/Ara.js
+++ js/Ara.js
@@ -9,34 +9,66 @@ $(function() {
 
 	//Mascara Jquery
 	$("#foneContato").mask("(99) 9999-9999");
-	
+
 
 	//Regras de validação dos campos do form
-    $("#formContato").validate({ //usa o name dos campos do form
+  $("#formContato").validate({ //usa o name dos campos do form
 		ignore:'',
-        rules: {
-            emailContato:{required: true, email:true},
-            nomeContato:{required: true, minlength: 4},
+    rules: {
+    	emailContato:{required: true, email:true},
+      nomeContato:{required: true, minlength: 4},
 			subjectContato:{required: true},
-            foneContato:{required: true},
+      foneContato:{required: true},
 			msgContato:{required: true}
-        },
-
-        messages: {
-        	emailContato:{required: "Informe o email", email:"Email inválido"},
-        	nomeContato:{required: "*Informe o nome",
-               minlength:"*É nome, animal não é apelido não menos 4 caracteres"},
+    },
+    messages: {
+      emailContato:{required: "Informe o email", email:"Email inválido"},
+      nomeContato:{required: "*Informe o nome",
+      minlength:"*É nome, animal não é apelido não menos 4 caracteres"},
 			subjectContato:{required: "*Escolhe alguma opção brother, assim tu não coopera"},
-            foneContato:{required: "*Manda o numero ai pra gente conversar no Whats"},
-            msgContato:{required:"*Digite alguma coisa ai mano, qual foi..."}
-        },
-
+      foneContato:{required: "*Manda o numero ai pra gente conversar no Whats"},
+      msgContato:{required:"*Digite alguma coisa ai mano, qual foi..."}
+		},
+		//add menssagem de erro
 		errorPlacement: function ($erroFormContato, $element) {
             var nome = $element.attr("name");
             $("#erro_" + nome).append($erroFormContato);
-        }
+					}
+  });
 
-    });
+	//Regras de validação para os campos de novo documentário
+	$("#formNewDoc").validate({
+		ignore:'',
+		rules: {
+			nomeDoc:{required: true, maxlength:20},
+			estudioDoc:{required: true},
+			autorDoc:{required: true},
+			dateDoc:{required: true},
+			generoDoc:{required: true},
+			idiomaDoc:{required: true},
+			//elsePort:{required: true},
+			sinopseDoc:{required: true, minlength:20},
+			thumbDoc:{required: true},
+			linkDoc:{required: true, url: true}
+		},
+		messages: {
+			nomeDoc:{required: "Informe o nome do documentário", maxlength:"Nome muito longo (<20)"},
+			estudioDoc:{required: "Informe o estudio"},
+			autorDoc:{required: "Informe o nome do autor"},
+			dateDoc:{required: "Informe a data"},
+			generoDoc:{required: "Informe o genero"},
+			idiomaDoc:{required: "informe o idioma do documentário"},
+			//elsePort:{required: true},
+			sinopseDoc:{required: "Informe a sinopse", minlength:"Sinopse muito curta"},
+			thumbDoc:{required: "Adicione uma imagem miniatura"},
+			linkDoc:{required: "Adicione o link do documentário", url: "URL invalida"}
+		},
+		//add menssagem de erro
+		errorPlacement: function ($erroFormDoc, $element) {
+						var nome = $element.attr("name");
+						$("#erro_" + nome).append($erroFormDoc);
+					}
+	});
 
     //Limpar campos de um form quando o usuário clicar no botão Limpar
     $(".btlimpar").click(function(){
@@ -47,8 +79,8 @@ $(function() {
     });
 })
 
-//Graficos dashboard
 
+//Graficos dashboard
 var randomScalingFactor = function() {
 		return Math.round(Math.random() * 1000);
 };
